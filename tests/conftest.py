897d6aa3020445c2931d1d081b21ac83b0da116c
Tatiana Konischeva <moyzaldi@gmail.com>
2024-12-19 01:03:50 +0300

    Добавлен webdriver  в Conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,11 +7,11 @@ from selenium import webdriver
 @pytest.fixture()
 def browser_settings():
     with allure.step("Параметры браузера"):
-        # driver_options = webdriver.ChromeOptions()
-        # driver_options.page_load_strategy = 'eager'
-        # browser.config.driver_options = driver_options
-        # browser.config.window_height = 1080
-        # browser.config.window_width = 1920
+        driver_options = webdriver.ChromeOptions()
+        driver_options.page_load_strategy = 'eager'
+        browser.config.driver_options = driver_options
+        browser.config.window_height = 1080
+        browser.config.window_width = 1920
         browser.config.base_url = 'https://demoqa.com'
         browser.config.timeout = 20
 
